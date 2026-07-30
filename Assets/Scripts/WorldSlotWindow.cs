18b0019c963fcac3fcd56a8a91bb256233cf748b
KAYVERIN <35387911+KAYVERIN@users.noreply.github.com>
2026-07-30 13:21:10 +0300

    Auto-save: 2026-07-30 13:21:10

--- Assets/Scripts/WorldSlotWindow.cs
+++ Assets/Scripts/WorldSlotWindow.cs
@@ -191,14 +191,14 @@ public class WorldSlotWindow : MonoBehaviour
     {
         if (slotBackground == null) return;
 
-        if (highlight)
+      /*  if (highlight)
         {
             slotBackground.color = new Color(1f, 1f, 0f, 0.5f);
         }
         else
         {
             slotBackground.color = Color.white;
-        }
+        }*/
     }
 
     private void Log(string message)
