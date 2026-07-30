0dfd602989bad18be7205f5a673747e3daa0f07a
KAYVERIN <35387911+KAYVERIN@users.noreply.github.com>
2026-07-30 14:07:46 +0300

    Auto-save: 2026-07-30 14:07:46

--- Assets/Scripts/UI/Windows/DragWorldWindow.cs
+++ Assets/Scripts/UI/Windows/DragWorldWindow.cs
@@ -75,7 +75,7 @@ public class DragWorldWindow : MonoBehaviour
         Ray ray = mainCamera.ScreenPointToRay(mousePos);
 
         // Получаем все попадания по слоям slots и cards
-        int layerMask = (1 << LayerMask.NameToLayer("slots")) | (1 << LayerMask.NameToLayer("cards"));
+        int layerMask = (1 << LayerMask.NameToLayer("Slots")) | (1 << LayerMask.NameToLayer("Cards"));
         RaycastHit2D[] hits = Physics2D.RaycastAll(ray.origin, ray.direction, 100f, layerMask);
 
         if (hits.Length > 0)
