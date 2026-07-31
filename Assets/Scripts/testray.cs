c642f5a0aaaea627a38d974c99ae5dd429b52677
KAYVERIN <35387911+KAYVERIN@users.noreply.github.com>
2026-07-31 15:46:24 +0300

    Auto-save: 2026-07-31 15:46:24

--- Assets/Scripts/testray.cs
+++ Assets/Scripts/testray.cs
@@ -4,24 +4,31 @@ public class Raycast2DPerspective : MonoBehaviour
 {
     void Update()
     {
-        // 1. Создаем 3D луч от камеры через курсор
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        // 2. Преобразуем 3D луч в 2D (берем только XY компоненты)
-        Vector2 rayOrigin = ray.origin;
-        Vector2 rayDirection = ray.direction;
+        // Рисуем луч в 3D (зеленый)
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.green, 1f);
 
-        // 3. Пускаем 2D луч
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, 100f);
+        // Находим пересечение с Z = 0
+        Plane plane = new Plane(Vector3.forward, Vector3.zero);
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 hitPoint = ray.GetPoint(distance);
 
-        if (hit.collider != null)
-        {
-            Debug.Log("Попали в 2D объект: " + hit.collider.gameObject.name);
-            Debug.DrawLine(ray.origin, hit.point, Color.red, 1f);
-        }
-        else
-        {
-            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.green, 1f);
+            // Рисуем красную точку на Z = 0
+            Debug.DrawLine(ray.origin, hitPoint, Color.red, 1f);
+
+            // Проверяем, есть ли там 2D коллайдер
+            RaycastHit2D hit2D = Physics2D.Raycast(hitPoint, Vector2.zero, 0.01f);
+            if (hit2D.collider != null)
+            {
+                Debug.Log("Есть коллайдер под курсором!");
+            }
+            else
+            {
+                Debug.Log("Под курсором нет коллайдера на Z=0");
+            }
         }
     }
 }
